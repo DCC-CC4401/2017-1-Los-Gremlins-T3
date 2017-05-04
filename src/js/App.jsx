8fcb7ef25b841c1e3a47129275cf8bb41b80cb26
Pablo Reszczynski <pablore@me.com>
2017-05-04 02:50:50 -0300

    Added App.jsx content

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
 
 class Main extends Component {
-  constructor() {
-    super();
-  }
-
   render() {
-    <h1>Hello, World!</h1>
+    return (
+      <h1>Hello, World!</h1>
+    );
   }
 }
+
+ReactDOM.render(
+  <Main>
+  </Main>
+  , document.getElementById('app'));
