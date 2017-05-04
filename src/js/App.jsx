428b1e6bb563a50853226dd8406738c6e6eab7af
Pablo Reszczynski <pablore@me.com>
2017-05-04 02:18:31 -0300

    Added App.jsx * *

---
+++ src/js/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+
+class Main extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    <h1>Hello, World!</h1>
+  }
+}
