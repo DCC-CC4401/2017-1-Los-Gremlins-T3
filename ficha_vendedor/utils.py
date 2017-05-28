cc7e8bf7979bd8e226ed412379f9ed17cfaa7ca6
druzmieres <druzmieres@gmail.com>
2017-05-28 19:47:12 -0400

    Agrega metodo vendedor activo

---
+++ ficha_vendedor/utils.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+actual_hour = datetime.now().hour
+actual_minute = datetime.now().minute
+now = datetime.strptime(str(actual_hour) + ':' + str(actual_minute), '%H:%M').time()
+
+def check_fixed_seller_active(start, end):
+    start = datetime.strptime(start, '%H:%M').time()
+    end = datetime.strptime(end, '%H:%M').time()
+    if start <= end:
+        return start <= now <= end
+    else:
+        return start <= now or now <= end
+
+
+print (datetime.strptime('03:55', '%H:%M').time())
+print (check_fixed_seller_active('11:00', '12:00'))
+print (check_fixed_seller_active('12:30', '21:30'))
