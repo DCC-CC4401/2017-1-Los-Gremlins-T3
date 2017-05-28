94b955f60166e8fc161c583261b175402ab4247b
Pablo Reszczynski <pablore@me.com>
2017-05-28 16:01:11 -0400

    Fixed problems with file paths

--- producto/views.py
+++ producto/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from producto.models import Producto
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+
+def producto(request, pkid):
+    try:
+        page = Producto.objects.get(pk=pkid)
+        context = { 'page': page }
+    except ObjectDoesNotExist:
+        context = None
+    return render(request, 'gremlins/gestion-productos.html', context)
