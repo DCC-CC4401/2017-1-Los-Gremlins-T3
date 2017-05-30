961759af2178d1e871b2ad5cef91f0c5adf5de45
Pablo Reszczynski <pablore@me.com>
2017-05-29 20:30:19 -0400

    save

--- producto/views.py
+++ producto/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from producto.models import Producto
 from django.core.exceptions import ObjectDoesNotExist
 from producto.forms import CreateProductoForm
 from producto.models import Producto
 
 # Create your views here.
+
+def add_producto(request):
+
+    if not request.user.is_authenticated:
+        return redirect('index')
+
+    user_id = request.user.id
+
+    if request.method == 'POST':
+        form = CreateProductoForm(request.POST)
+        if form.is_valid():
+            producto = Producto.objects.create(
+                name=form.cleaned_data['name'],
+                price=form.cleaned_data['price'],
+                stock=form.cleaned_data['stock'],
+                description=form.cleaned_data['description'],
+                owner=user_id,
+                photo=form.cleaned_data['img'],
+                prev_img_id=form.cleaned_data['prev_img']
+            )
+    else:
+        form = CreateProductoForm()
+
+    return render(request, 'producto/gestion-productos.html', {'form': form,
+                                                               'uid': user_id})
 
 def producto(request, pkid):
     try:
